02bfc379906b23f5d33f4baccccefe42ec13bac4
pradhan-pawan <pradhan.pawan98@gmail.com>
2026-03-24 20:29:02 +0530

    Added sql_soln

---
+++ sales_analysis/scripts/sql_solution.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+from utils import get_connection, save_to_csv
+
+def run_sql_solution(db_path, output_path):
+    conn = get_connection(db_path)
+    if conn is None:
+        return
+
+    query = """
+    SELECT 
+        c.customer_id AS Customer,
+        c.age AS Age,
+        i.item_name AS Item,
+        SUM(o.quantity) AS Quantity
+    FROM customer c
+    JOIN sales s ON c.customer_id = s.customer_id
+    JOIN orders o ON s.sales_id = o.sales_id
+    JOIN items i ON o.item_id = i.item_id
+    WHERE c.age BETWEEN 18 AND 35
+    AND o.quantity IS NOT NULL
+    GROUP BY c.customer_id, c.age, i.item_name
+    ORDER BY Customer, Item;
+    """
+
+    try:
+        df = pd.read_sql_query(query, conn)
+        df["Quantity"] = df["Quantity"].astype(int)
+        save_to_csv(df, output_path)
+    except Exception as e:
+        print(f"Query Error: {e}")
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    db_path = os.path.join(BASE_DIR, "sales.db")
+    output_path = os.path.join(BASE_DIR, "output", "sql_output.csv")
+
+    run_sql_solution(db_path, output_path)
